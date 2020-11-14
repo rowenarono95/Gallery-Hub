77e097ae09c50ecb8791db350243bb5c229360eb
rowenarono95 <rowenarono@gmail.com>
2020-11-14 13:11:28 +0300

    created superuser

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pichub.apps.PichubConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pichub/admin.py
+++ pichub/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Image
 
 # Register your models here.
+
+admin.site.register(Image)
