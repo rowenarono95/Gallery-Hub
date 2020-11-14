f15fcb1624ed3246d0d80449528aa3beac502cf4
rowenarono95 <rowenarono@gmail.com>
2020-11-14 12:50:03 +0300

    create db

--- pichub/models.py
+++ pichub/models.py
@@ -8,6 +8,6 @@ from django.db import models
 class Image(models.Model):
     name =models.CharField(max_length = 30)
     photo_description = models.TextField()
-    photo_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
-    photo_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
+    # photo_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
+    # photo_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
     photo = models.ImageField(upload_to = 'images/', default='image.jpg')
