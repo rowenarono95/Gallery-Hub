df4d51d3b72d77ea8607098d869557caa8f39bc1
rowenarono95 <rowenarono@gmail.com>
2020-11-14 12:07:59 +0300

    add class Image

--- pichub/models.py
+++ pichub/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+ 
+class Image(models.Model):
+    name =models.CharField(max_length = 30)
+    photo_description = models.TextField()
+    photo_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
+    photo_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
+    photo = models.ImageField(upload_to = 'images/', default='image.jpg')
