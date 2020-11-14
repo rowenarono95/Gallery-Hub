785724e577a18c48e99eadacc358fcbf91a7a2f1
rowenarono95 <rowenarono@gmail.com>
2020-11-14 15:01:25 +0300

    create class category

--- pichub/models.py
+++ pichub/models.py
@@ -9,9 +9,20 @@ class Image(models.Model):
     name =models.CharField(max_length = 30)
     image_description = models.TextField()
     image_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
-    # image_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
+    image_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
     image = models.ImageField(upload_to = 'images/', default='image.jpg')
 
 
 class Location(models.Model):
     location_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.location_name
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length = 30) 
+
+    
+    def __str__(self):
+        return self.category_name   
