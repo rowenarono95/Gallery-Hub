8e0b283c1e54dad43fa5d2c744e3ab071d953088
rowenarono95 <rowenarono@gmail.com>
2020-11-14 15:50:50 +0300

    create method image

--- pichub/views.py
+++ pichub/views.py
@@ -2,8 +2,13 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image
 
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the pichub')
+
+def image(request):
+    image = Image.objects.all()
+    return render(request, 'pic.html', {"image" : image})    
