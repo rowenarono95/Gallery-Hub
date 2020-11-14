4f4f4776421466a31e62a581fcc5169cb5c8a820
rowenarono95 <rowenarono@gmail.com>
2020-11-14 14:51:11 +0300

    create class location

--- pichub/models.py
+++ pichub/models.py
@@ -7,7 +7,11 @@ from django.db import models
  
 class Image(models.Model):
     name =models.CharField(max_length = 30)
-    photo_description = models.TextField()
-    # photo_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
-    # photo_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
-    photo = models.ImageField(upload_to = 'images/', default='image.jpg')
+    image_description = models.TextField()
+    image_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
+    # image_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
+    image = models.ImageField(upload_to = 'images/', default='image.jpg')
+
+
+class Location(models.Model):
+    location_name = models.CharField(max_length = 30)
