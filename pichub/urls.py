23a71ca6624cf58fdecc6b638238a879906548b5
rowenarono95 <rowenarono@gmail.com>
2020-11-14 17:08:46 +0300

    add navbar

--- pichub/urls.py
+++ pichub/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.image,name = 'Image'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
