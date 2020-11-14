03faa7515481f454b96b4ddf66ea5b22768e14e7
rowenarono95 <rowenarono@gmail.com>
2020-11-14 15:11:58 +0300

    add bootstrap

--- pichub/admin.py
+++ pichub/admin.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Image
+from .models import Image,Location,Category
 
 # Register your models here.
 
 admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
