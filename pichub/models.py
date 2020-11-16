3c02728f72ad2deb532fb2238592f2bfeaf60efa
rowenarono95 <rowenarono@gmail.com>
2020-11-16 08:49:26 +0300

    add heading

--- pichub/models.py
+++ pichub/models.py
@@ -52,7 +52,6 @@ class Category(models.Model):
     def delete_category_name(self):
         self.delete()         
 
-
 def update_image(self, Name=None, category=None):
         self.name = Name if Name else self.Name
         self.image_category = category if category else self.image_category 
